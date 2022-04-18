7617039611558cbcf3ac76a27e7341fa52864c45
Nabil Affo <nabil.noble01@gmail.com>
2022-04-18 17:50:19 +0000

    Update formatspecifiersfunctions.c

--- formatspecifiersfunctions.c
+++ formatspecifiersfunctions.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+
+/* TASK 0 FUNCTIONS */
+
 /**
  * print_char - prints a character
  * @ap: arguments of type va_list
@@ -44,4 +47,37 @@ int print_percent_symbol(va_list ap)
 
 	_putchar('%');
 	return (1);
+}
+
+/* TASK 1 FUNCTIONS */
+/**
+ * print_integer - prints an integer
+ * @ap: arguments of type va_list
+ * Return: the number of characters printed
+ */
+int print_integer(va_list ap)
+{
+	int num_length;
+
+	num_length = print_number(ap);
+	return (num_length);
+}
+
+/**
+ * unsigned_integer - prints Unsigned integers
+ * @ap: arguments of type va_list
+ * Return: the number of characters printed
+ */
+int unsigned_integer(va_list ap)
+{
+	unsigned int num;
+
+	num = va_arg(ap, unsigned int);
+
+	if (num == 0)
+		return (print_unsgined_number(num));
+
+	if (num < 1)
+		return (-1);
+	return (print_unsgined_number(num));
 }
